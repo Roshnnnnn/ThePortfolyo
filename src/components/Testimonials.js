07badd152e0f070a1bfb80531e9227ef267b5e2f
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-03-18 15:00:55 +0530

    update testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,27 +1,93 @@
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { fetchData } from "../../pages/api/hello";
 
 const Testimonials = () => {
-  const [user, setUser] = useState([]);
-  const [active, setActive] = useState({});
+  const [testimonials, setTestimonials] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
-    const fetchUserData = async () => {
+    const fetchTestimonials = async () => {
       try {
         const data = await fetchData();
-        console.log(data.testimonials);
-        setUser(data.testimonials);
-        if (data.testimonials.length > 0) {
-          setActive(data.testimonials[0]);
-          console.log(data.testimonials[0]);
-        }
+        setTestimonials(data.testimonials);
       } catch (error) {
-        console.log(error.message);
+        console.log("Error fetching testimonials:", error.message);
       }
     };
-    fetchUserData();
+
+    fetchTestimonials();
   }, []);
-  return <div>Testimonials</div>;
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="tonni_tm_section">
+      <div className="tonni_tm_testimonials">
+        <div className="container">
+          <div
+            className="testimonials_inner wow fadeInUp"
+            data-wow-duration="1s"
+          >
+            <span className="shape">
+              <img className="svg anim_circle" src="img/svg/icon9.svg" alt="" />
+            </span>
+            <span className="shape2">
+              <img className="svg" src="img/svg/icon12.svg" alt="" />
+            </span>
+            <div className="leftpart">
+              <div className="active-image">
+                <img
+                  src={testimonials[activeIndex]?.image.url}
+                  alt={testimonials[activeIndex]?.name}
+                />
+              </div>
+            </div>
+            <div className="rightpart">
+              <div className="active-review">
+                <p>{testimonials[activeIndex]?.review}</p>
+              </div>
+              <div className="details">
+                <div className="infos">
+                  <div className="avatars">
+                    <ul className="grid-sort">
+                      {testimonials.map(
+                        (item, index) =>
+                          index !== activeIndex && (
+                            <li key={index} className={` grid-item ${index}`}>
+                              <span onClick={() => handleItemClick(index)}>
+                                <div
+                                  data-img-url={item && item.image.url}
+                                  style={{
+                                    backgroundImage: `url(${
+                                      item && item.image.url
+                                    })`,
+                                  }}
+                                />
+                              </span>
+                              <span className="hidden">hlo</span>
+                            </li>
+                          )
+                      )}
+                    </ul>
+                  </div>
+                  <div className="short">
+                    <div className="name">
+                      <div>
+                        <h3>{testimonials[activeIndex]?.name}</h3>
+                        <span>{testimonials[activeIndex]?.position}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
